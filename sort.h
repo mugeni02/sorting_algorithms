53af97e7869db6905857987047aeefdaa5435ee7
mugeni02 <mugenir7@gmail.com>
2024-02-17 10:47:53 -0800

    correction

--- sort.h
+++ sort.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @value: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s {
+    int value;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
@@ -16,38 +29,10 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, int low, int cnt, int dir);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
+/*void sort_deck(deck_node_t **deck);*/
+void print_list(listint_t *list);
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-	const int n;
-
-
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size);
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list);
 
 
 #endif
+
